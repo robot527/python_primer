cd2cdf99bc7cf7025ed88469395f3e05d2e5fb5b
robot527 <luoyaozu@foxmail.com>
2014-11-29 12:53:20 +0800

    Create sketch_write_to_file.py

---
+++ sketch_write_to_file.py
@@ -0,0 +1,46 @@
+""" This a program which used to deal with
+    the test file called "sketch.txt".
+    And then, write the data which disposed
+    from sketch.txt to two target file.
+"""
+
+import os
+import sys
+print( os.getcwd() )
+#os.chdir( 'the dir which include the file sketch.txt' )
+
+try:
+    data = open( 'sketch.txt' )
+except FileNotFoundError:
+    print( 'File not found in the current working directory!' )
+    sys.exit();
+
+man = []
+other = []
+    
+for each_line  in data:
+    try:
+        ( role, spoken ) = each_line.split( ':', 1 )
+        spoken = spoken.strip()
+        if role == 'Man':
+            man.append( spoken )
+        elif role == 'Other Man':
+            other.append( spoken )
+    except ValueError:
+        pass
+
+data.close()
+
+
+try:
+    man_data = open( 'man_data.txt', 'w' )
+    other_data = open( 'other_data.txt', 'w' )
+    
+    print( man, file = man_data )
+    print( other, file = other_data )
+    
+    man_data.close()
+    other_data.close()
+except IOError:
+    print( 'File error' )
+
