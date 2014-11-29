d45b8c3d1156521179e5c297e7d28c61b7660625
robot527 <luoyaozu@foxmail.com>
2014-11-29 20:37:29 +0800

    Update sketch_write_to_file.py

--- sketch_write_to_file.py
+++ sketch_write_to_file.py
@@ -7,7 +7,7 @@
 import os
 import sys
 print( os.getcwd() )
-#os.chdir( 'the dir which include the file sketch.txt' )
+#os.chdir( 'the directory which include the file sketch.txt' )
 
 try:
     data = open( 'sketch.txt' )
@@ -17,7 +17,26 @@ except FileNotFoundError:
 
 man = []
 other = []
-    
+
+def print_nested_list_V3( alist, indent = False, level = 0, fh = sys.stdout ):
+    """ This function takes one positional argument call "alist" which is
+        any Python list (of possibly nested list), and two optional arguments
+        called "indent" and "level". The indent controls print style, False
+        means disable indent, True means enable indent. The level controls
+        the amount of indentation for each item in in the provided list.
+        Each data item in the provided list is (recursively) printed to the
+        screen on it's own line. Added a fourth argument "fh" with a default
+        value to print_nested_list_V2() function to identify a place to write
+        data to.
+    """
+    for item in alist:
+        if isinstance( item, list ):
+            print_nested_list_V3( item, indent, level + 1, fh )
+        else:
+            if indent:
+                print( '\t' * level, end = '', file = fh )
+            print( item, file = fh )
+
 for each_line  in data:
     try:
         ( role, spoken ) = each_line.split( ':', 1 )
@@ -36,8 +55,8 @@ the code without the “finally” suite.
 """
 try:
 	with open( 'man_data.txt', 'w' ) as man_file:
-		print( man, file = man_file )
+		print_nested_list_V3( man, False, 0, man_file )
 	with open( 'other_data.txt', 'w' ) as other_file:
-		print( other, file = other_file )
+		print_nested_list_V3( other, fh = other_file )
 except IOError as err:
 	print( 'File error: ' + str( err ) )
