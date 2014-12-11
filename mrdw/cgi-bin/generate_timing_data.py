6645b4368a28788b69d77a2ff242637f2800d697
robot527 <luoyaozu@foxmail.com>
2014-12-11 17:02:17 +0800

    Create generate_timing_data.py

---
+++ mrdw/cgi-bin/generate_timing_data.py
@@ -0,0 +1,18 @@
+
+#! /usr/local/bin/python3
+
+import cgi
+import athletemodel
+import yate
+
+athletes = athletemodel.get_from_store()
+
+form_data = cgi.FieldStorage()
+athlete_name = form_data['which_athlete'].value
+
+print(yate.start_response())
+print(yate.include_header("Coach Kelly's Timing data"))
+print(yate.header("Athlete: " + athlete_name + ", DOB: " + athletes[athlete_name].dob + "."))
+print(yate.para("The top times for this athlete are:"))
+print(yate.u_list(athletes[athlete_name].top3))
+print(yate.include_footer({"Home" : "/index.html", "Select another athele" : "generate_list.py"}))
