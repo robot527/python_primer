c095db2c1f1d069f6b144d9f48b447d871816b82
robot527 <luoyaozu@foxmail.com>
2015-03-05 21:22:28 +0800

    updated fibo.py

--- fibo.py
+++ fibo.py
@@ -1,7 +1,7 @@
 #! usr/bin/python3
 
-''' Two different implementations of fibonacci sequence
-have different performance. '''
+''' Different implementations of fibonacci sequence
+have different performances. '''
 
 import time
 
@@ -20,6 +20,30 @@ def fib2(n):
     res = fib2(n - 1) + fib2(n - 2)
     known[n] = res
     return res
+
+
+def fib3(n):
+    return n < 2 and 1 or fib3(n - 1) + fib3(n - 2)
+
+
+fib4 = lambda n: n if n < 2 else fib4(n - 1) + fib4(n - 2)
+
+
+def fib5(n):
+    x, y = 0, 1
+    while(n):
+        x, y, n = y, x + y, n - 1
+    return x
+
+
+def fib6(n):
+    def fib_iter(n, x, y):
+        if 0 == n: return x
+        else: return fib_iter(n - 1, y, x + y)
+    return fib_iter(n, 0, 1)
+
+
+fib7 = lambda n, x = 0,y = 1: x if not n else fib7(n - 1, y, x + y)
 
 
 def test_fib(func, para):
@@ -45,4 +69,6 @@ if __name__ == '__main__':
     num = 32
     print('fib1(%d) = %d\n' % (num, test_fib(fib1, num)))
     print('fib2(%d) = %d\n' % (num, test_fib(fib2, num)))
+    print('fib5(%d) = %d\n' % (num, test_fib(fib5, num)))
+    print('fib7(%d) = %d\n' % (num, test_fib(fib7, num)))
     fib_show(num + 1)
