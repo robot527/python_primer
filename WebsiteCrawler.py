b7f0d181ae5e951bbf98f4bbf352e32cb1374c52
robot527 <luoyaozu@foxmail.com>
2015-04-07 20:41:42 +0800

    updated files

--- WebsiteCrawler.py
+++ WebsiteCrawler.py
@@ -1,9 +1,12 @@
 #! /usr/bin/python2.7
-import re, urllib
+import re
+import urllib
 
-textfile = open('depth_1.txt', 'wt') #The preferred way to open a file is with the builtin open() function.
+
+#The preferred way to open a file is with the builtin open() function.
+textfile = open('depth_1.txt', 'wt')
 print "Enter the URL you wish to crawl.."
-print 'Usage  - "http://phocks.org/stumble/creepy/" <-- With the double quotes'
+print 'Usage - "http://phocks.org/stumble/creepy/" <-- With the double quotes'
 myurl = input("@> ")
 """
 findall(pattern, string, flags=0)
@@ -12,9 +15,11 @@ urlopen(url, data=None, proxies=None)
     Create a file-like object for the specified URL to read from.
 re.I is an abbreviation of re.IGNORECASE
 """
-for i in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(myurl).read(), re.I):
-        print i  
-        for ee in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(i).read(), re.I):
-                print ee
-                textfile.write(ee+'\n')
+for i in re.findall('''href=["'](.[^"']+)["']''',\
+                    urllib.urlopen(myurl).read(), re.I):
+    print i
+    for ee in re.findall('''href=["'](.[^"']+)["']''',\
+                        urllib.urlopen(i).read(), re.I):
+        print ee
+        textfile.write(ee+'\n')
 textfile.close()
--- nester.py
+++ nester.py
@@ -1,22 +1,23 @@
+#! /usr/bin/python3
 """ This is the "nester.py" module and id provides two function called
   print_nested_list() and print_nested_list_V2() which print lists
   that may or may not include nested lists.
 """
 
 
-#the argument with default value is a optional argument.
-def print_nested_list( alist, indent = False, level = -1 ):
-     if True == isinstance( alist, list ):
-         for item in alist:
-             print_nested_list( item, indent, level + 1 )
-     else:
-         if indent:
-             for tab_stop in range( level ):
-                    print( "\t", end = '') #print a TAB for indent
-         print( alist )
+def print_nested_list(alist, indent=False, level=-1):
+    """print Python list (of possibly nested list)"""
+    if True == isinstance(alist, list):
+        for item in alist:
+            print_nested_list(item, indent, level + 1)
+    else:
+        if indent:
+            for tab_stop in range(level):
+                print("\t", end='') #print a TAB for indent
+        print(alist)
 
 
-def print_nested_list_V2( alist, indent = False, level = 0 ):
+def print_nested_list_V2(alist, indent=False, level=0):
     """ This function takes one positional argument call "alist" which is
         any Python list (of possibly nested list), and two optional arguments
         called "indent" and "level". The indent controls print style, False
@@ -26,39 +27,37 @@ def print_nested_list_V2( alist, indent = False, level = 0 ):
         screen on it's own line.
     """
     for item in alist:
-        if isinstance( item, list ):
-            print_nested_list_V2( item, indent, level + 1 )
+        if isinstance(item, list):
+            print_nested_list_V2(item, indent, level + 1)
         else:
             if indent:
-                """ a terse way replaces the statements
-                " for tab_stop in range( level ):
-                      print( '\t', end = '') "
-                """
-                print( '\t' * level, end = '' )
-            print( item )
+                print('\t' * level, end='')
+            print(item)
 
 
 
-test_list = ['The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91,
-          ['Graham Chapman',
-           ['Michael Palin', 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']
-          ],
-          'added by robot527',
-          [21, 32, 55],
-          (2, 3, 4),
-          [1.23, ['float', 'double']],
-          "python 3.4.2"
-         ]
+test_list = [
+    'The Holy Grail', 1975, 'Terry Jones & Terry Gilliam', 91,
+    [
+        'Graham Chapman',
+        ['Michael Palin', 'John Cleese', 'Terry Gilliam', 'Eric Idle', 'Terry Jones']
+    ],
+    'added by robot527',
+    [21, 32, 55],
+    (2, 3, 4),
+    [1.23, ['float', 'double']],
+    "python 3.4.2"
+    ]
 
 #test code
-print( test_list )
-print( '\n' )
+print(test_list)
+print('\n')
 
-print_nested_list( test_list )
-print( '\n' )
+print_nested_list(test_list)
+print('\n')
 
-print_nested_list_V2( test_list, True )
-print( '\n' )
+print_nested_list_V2(test_list, True)
+print('\n')
 
-print_nested_list_V2( test_list, True, 2 )
-print( '\n' )
+print_nested_list_V2(test_list, True, 2)
+print('\n')
