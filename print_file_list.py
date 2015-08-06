b128d8e312aa9f7885d9b276d86635b3af9a8ec3
robot527 <luoyaozu@foxmail.com>
2015-08-06 23:53:06 +0800

    added print_file_list.py

---
+++ print_file_list.py
@@ -0,0 +1,32 @@
+#! usr/bin/python
+
+def file_travesal(dirtectory='.', file_list=[]):
+    '''
+    Get file list from the directory including files in its subdirectories.
+    '''
+    import os
+    file_list += [os.path.join(dirtectory, f) for f in os.listdir(dirtectory)
+                if os.path.isfile(os.path.join(dirtectory, f))]
+    for item in os.listdir(dirtectory):
+        if os.path.isdir(os.path.join(dirtectory, item)):
+            file_travesal(os.path.join(dirtectory, item), file_list)
+
+if __name__ == '__main__':
+    print 'Please input a directory.'
+    print 'Usage - "..", "/home/user/test/", default is "."'
+    target_dir = raw_input("@> ")
+    if target_dir is '':
+        target_dir = '.'
+    print 'Target directory is ' + target_dir + '  !\n'
+    file_lst = []
+    file_travesal(target_dir, file_lst)
+    #Get .h file list
+    h_file_list = [ef for ef in file_lst if ef[-2:] == '.h']
+    #Get .c file list
+    c_file_list = [ef for ef in file_lst if ef[-2:] == '.c']
+    for each_item in file_lst:
+        print each_item
+    print '\t\t Total file number is %d !!!\n' % len(file_lst)
+    print h_file_list
+    print
+    print c_file_list
